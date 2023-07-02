891b0c7c421a78115cab5d1036ea45ad7c58e0ca
The-Arabi <100914809+The-Arabi@users.noreply.github.com>
2023-07-02 18:18:09 +0300

    Finish project

--- javascript.js
+++ javascript.js
@@ -1,6 +1,19 @@
 let grid = document.querySelector(".grid");
-grid.style.gridTemplateColumns = "repeat(16, 1fr)";
-grid.style.gridTemplateRows = "repeat(16, 1fr)";
-for (i = 0; i < 16*16; i++) grid.innerHTML = grid.innerHTML + '<div class="grid-item"></div>'
-let gridItems = document.querySelectorAll(".grid-item");
-gridItems.forEach(gridItem => gridItem.addEventListener("mouseover", () => gridItem.style.backgroundColor = "black"))
+let gridItems;
+let button = document.querySelector("button");
+function createGrid(dim = 16) {
+    if (isNaN(dim) || dim > 100) {
+        do {
+            dim = prompt("give the grid dimenstions")
+        }
+        while(isNaN(dim) || dim > 100)
+    }
+    grid.style.gridTemplateColumns = `repeat(${dim}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${dim}, 1fr)`;
+    grid.innerHTML = null;
+    for (i = 0; i < dim**2; i++) grid.innerHTML = grid.innerHTML + '<div class="grid-item"></div>';
+    gridItems = document.querySelectorAll(".grid-item");
+    gridItems.forEach(gridItem => gridItem.addEventListener("mouseover", () => gridItem.style.backgroundColor = "black"));
+}
+createGrid();
+button.addEventListener("click", createGrid);
